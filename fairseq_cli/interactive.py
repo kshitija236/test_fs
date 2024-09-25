0921daa718408dc2fb03f4c618d1b0fd98aa3864
Kshitij Agrawal <kshiagra@qti.qualcomm.com>
2024-09-25 11:27:33 +0530

    Add test comment

--- fairseq_cli/interactive.py
+++ fairseq_cli/interactive.py
@@ -3,6 +3,9 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+#Adding test comment
+
 """
 Translate raw text with a trained model. Batches data on-the-fly.
 """
